cadddb430e60f6c81da678379eacf7e079cca4f6
elsizzz <d4lv09@yandex.ru>
2026-03-14 20:47:08 +0300

    Convert OneWayBindingViewModel to Toolkit MVVM

--- BindingDemoMVVM/ViewModels/OneWayBindingViewModel.cs
+++ BindingDemoMVVM/ViewModels/OneWayBindingViewModel.cs
@@ -1,37 +1,10 @@
-using System;
-using System.Windows.Threading;
+using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace BindingDemoMVVM.ViewModels
 {
-    /// <summary>
-    /// ViewModel для демонстрации односторонней привязки (OneWay).
-    /// CurrentTime обновляется по таймеру.
-    /// </summary>
-    public class OneWayBindingViewModel : ViewModelBase, IDisposable
+    public partial class OneWayBindingViewModel : ObservableObject
     {
-        private string _currentTime;
-        private readonly DispatcherTimer _timer;
-
-        public OneWayBindingViewModel()
-        {
-            _currentTime = DateTime.Now.ToString("HH:mm:ss");
-            _timer = new DispatcherTimer
-            {
-                Interval = TimeSpan.FromSeconds(1)
-            };
-            _timer.Tick += (s, e) => CurrentTime = DateTime.Now.ToString("HH:mm:ss");
-            _timer.Start();
-        }
-
-        public string CurrentTime
-        {
-            get => _currentTime;
-            set => SetProperty(ref _currentTime, value);
-        }
-
-        public void Dispose()
-        {
-            _timer?.Stop();
-        }
+        [ObservableProperty]
+        private int progressValue = 25;
     }
 }
