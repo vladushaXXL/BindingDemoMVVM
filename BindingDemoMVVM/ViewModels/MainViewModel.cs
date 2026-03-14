4d4b1d4f03cfa91dfff3c086d7d4122d737d1986
elsizzz <d4lv09@yandex.ru>
2026-03-14 14:13:48 +0300

    Connect MainViewModel to MainWindow

--- BindingDemoMVVM/ViewModels/MainViewModel.cs
+++ BindingDemoMVVM/ViewModels/MainViewModel.cs
@@ -1,12 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace BindingDemoMVVM.ViewModels
 {
-    internal class MainViewModel
+    public class MainViewModel : INotifyPropertyChanged
     {
+
+        private string _textValue;
+
+        public string TextValue
+        {
+            get => _textValue;
+            set
+            {
+                _textValue = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private int _sliderValue;
+
+        public int SliderValue
+        {
+            get => _sliderValue;
+            set
+            {
+                _sliderValue = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
     }
 }
