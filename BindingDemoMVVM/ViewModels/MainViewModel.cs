ae15ee939ea9be9bd54a7d0c08d2c3e7dc6a9767
elsizzz <d4lv09@yandex.ru>
2026-03-14 14:13:02 +0300

    Add basic ViewModel implementation

---
+++ BindingDemoMVVM/ViewModels/MainViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BindingDemoMVVM.ViewModels
+{
+    internal class MainViewModel
+    {
+    }
+}
